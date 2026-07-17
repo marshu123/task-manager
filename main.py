d1a789397f72a6872e7249ccf3517132c054dbbf
Marshid P <marshimarshu007@gmail.com>
2026-07-17 15:38:23 +0530

    Add initial FastAPI application

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Task Manager API is running"}
+
+@app.get("/hello")
+def hello():
+    return {"message": "Hello, Marshid!"}
